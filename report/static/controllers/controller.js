8732951ce922b29585fa80370cfc09048cfbcd2a
Alexey <alexeygolovin5587>
2016-04-24 10:10:51 -0700

    dashboard

--- report/static/controllers/controller.js
+++ report/static/controllers/controller.js
@@ -12,7 +12,7 @@ function MainCtrl($scope) {
     $scope.d1_2 = [["Jan", 122],["Feb", 170],["Mar", 163],["Apr", 161],["May", 122],["Jun", 136],["Jul", 130],["Aug", 128],["Sep", 148]];
     $scope.d1_3 = [["Jan", 81],["Feb", 92],["Mar", 98],["Apr", 102],["May", 80],["Jun", 97],["Jul", 86],["Aug", 105],["Sep", 85]];
 
-
+		
 };
 
 function Login($scope, $http, $filter, $rootScope, $location) {
